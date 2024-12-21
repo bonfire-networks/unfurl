8369d7df27de6dba7f0dbc21243fc7f5e094f6f1
Mayel de Borniol <git@mayel.space>
2024-12-21 14:43:09 +0000

    configurable redirects

--- lib/fetcher/fetcher.ex
+++ lib/fetcher/fetcher.ex
@@ -3,7 +3,7 @@ defmodule Unfurl.Fetcher do
   A module for fetching body data for a given url
   """
   use Tesla
-  plug Tesla.Middleware.FollowRedirects, max_redirects: 5
+  plug Unfurl.Tesla.Middleware.MaybeFollowRedirects, max_redirects: 5
 
   import Untangle
 
